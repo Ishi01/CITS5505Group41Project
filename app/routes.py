08c87c07092aff67eb09470d98e9145b5f132933
stuar7 <stuartpm@gmail.com>
2024-04-17 14:21:13 +0800

    Question / Answer Prototype

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,27 @@
-from flask import render_template
+from flask import Flask, jsonify, render_template, request
+import random
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
+
+qa_dict = {
+    "Where is the Eiffel Tower found?": "France",
+    "Where is the Statue of Liberty found?": "United States",
+    "Where is the Taj Mahal found?": "India"
+}
+
+@app.route('/get-random-qa')
+def get_random_qa():
+    question, answer = random.choice(list(qa_dict.items()))
+    return jsonify(question=question)
+
+@app.route('/check-answer', methods=['POST'])
+def check_answer():
+    user_answer = request.json['answer']
+    question = request.json['question']
+    correct_answer = qa_dict[question]
+    is_correct = (user_answer.strip().lower() == correct_answer.strip().lower())
+    return jsonify(is_correct=is_correct)
---
+++ app/static/js/qa.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    var currentQuestion = '';
+
+    $('#getQAButton').click(function() {
+        $.ajax({
+            url: '/get-random-qa',
+            type: 'GET',
+            success: function(response) {
+                $('#question').text(response.question);
+                currentQuestion = response.question;
+                $('#answerInput').val(''); // Clear previous answer
+                $('#feedback').text(''); // Clear previous feedback
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    });
+
+    $('#submitAnswerButton').click(function() {
+        var userAnswer = $('#answerInput').val();
+        if (userAnswer) {
+            $.ajax({
+                url: '/check-answer',
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify({ answer: userAnswer, question: currentQuestion }),
+                success: function(response) {
+                    if (response.is_correct) {
+                        $('#feedback').text('Correct!');
+                    } else {
+                        $('#feedback').text('Incorrect. Try again!');
+                    }
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+        }
+    });
+});
