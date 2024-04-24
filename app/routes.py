c9cda522aeb2f3cd73b062e2799d116b78054194
berry1104 <935311076@qq.com>
2024-04-24 14:39:17 +0800

    merge app/routes.py

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ import random
 import os
 from app import app
 from app.forms import LoginForm
-from flask import flash, redirect
+from flask import flash, redirect, url_for
 
 @app.route('/')
 @app.route('/index')
@@ -88,5 +88,5 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
