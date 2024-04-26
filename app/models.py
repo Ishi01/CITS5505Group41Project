049b4e77007e328d706095620da2ba1d91ee8afc
berry1104 <935311076@qq.com>
2024-04-27 03:08:46 +0800

    database model

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
+from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+
 
 class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -11,5 +13,21 @@ class User(db.Model):
                                              unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
+    games: so.WriteOnlyMapped['Game'] = so.relationship(
+        back_populates='player')
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Game(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    result: so.Mapped[int] = so.mapped_column(sa.Integer)
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc))
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
+                                               index=True)
+
+    player: so.Mapped[User] = so.relationship(back_populates='games')
+
+    def __repr__(self):
+        return '<Game {}>'.format(self.result)
