fcaf59f0e164ef28007fcc4bb1433f59febc3409
berry1104 <935311076@qq.com>
2024-04-26 23:30:45 +0800

    configure shell context

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-from app import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db
+from app.models import User, Game
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Game': Game}
