0382281d4ff56acc1a78ee341f0c923dabda55e0
stuar7 <stuartpm@gmail.com>
2024-05-15 21:37:09 +0800

    Minor Changes

--- app/user.py
+++ app/user.py
@@ -15,20 +15,21 @@ from app.models import User, Game, UserGameHistory
 from flask_session import Session
 from flask_login import LoginManager
 
-
 user = Blueprint('user', __name__)
 
 import random
 from flask import render_template, current_app
 from os import path
 
-@user.route('/user/<user_identifier>')
-def user_profile(user_identifier):
-    # Check if the identifier is an integer (user ID) or a string (username)
-    if user_identifier.isdigit():
-        user = User.query.get(int(user_identifier))
+@user.route('/user/id/<int:user_id>')  # Route for user ID
+@user.route('/user/<user_name>')       # Route for username
+def user_profile(user_id=None, user_name=None):
+    if user_id is not None:
+        user = User.query.get(user_id)
+    elif user_name is not None:
+        user = User.query.filter_by(username=user_name).first()
     else:
-        user = User.query.filter_by(username=user_identifier).first()
+        return jsonify({'error': 'Invalid user identifier'}), 400
 
     if user is None:
         return jsonify({'error': 'User not found'}), 404
